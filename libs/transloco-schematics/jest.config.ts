9605da83a5f8727f5bd1728e5cf0a241ce1da5a3
stephaniekatterwe <stephanie.katterwe@dhl.com>
2024-08-06 16:23:01 +0200

    feat(transloco): 🎸 update replace-in-flight
    Update replace-in-flight to ^8.1.0 to resolve CWE-772 in inflight
    
    ✅ Closes: #768

--- libs/transloco-schematics/jest.config.ts
+++ libs/transloco-schematics/jest.config.ts
@@ -1,17 +1,19 @@
-import type {Config} from 'jest';
+import type { Config } from 'jest';
 
 export default {
   displayName: 'transloco-schematics',
   testEnvironment: 'node',
   globals: {},
   transform: {
-    '^.+\\.[tj]sx?$': [
-      'ts-jest',
+    '^.+.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
       {
         tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$',
       },
     ],
   },
+  transformIgnorePatterns: ['node_modules/?!(.\\*.mjs$|replace-in-file)'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/libs/transloco-schematics',
   preset: '../../jest.preset.js',
